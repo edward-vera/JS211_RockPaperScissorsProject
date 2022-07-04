154930be0e5f6f3c1743b5d947b82248ab57fd3e
edward-vera <103536879+edward-vera@users.noreply.github.com>
2022-07-04 11:00:36 -0500

    added a bunch of styles'
    git status
    lear
    clear

--- main.js
+++ main.js
@@ -1,92 +1,93 @@
-// uses strict mode so strings are not coerced, variables are not hoisted, etc... 
-'use strict';
-
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// the function that will be called by the unit test below
-const rockPaperScissors = (hand1, hand2) => {
-
-  // Should detect a tile
-  // Should detect which hand won
-  // Shrould scrub input to ensure lowercase with 'trim"ed whitespace
-  // Write code here
-
-  let userOne = hand1.toLowerCase().trim()
-  let userTwo = hand2.toLowerCase().trim()
-
-  if(userOne === userTwo)
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+const SELECTIONS = [
   {
-    return "It's a tie!"
+    name: 'rock',
+    emoji: '✊',
+    beats: 'scissors',
+  },
+  {
+    name: 'paper',
+    emoji: '✋',
+    beats: 'rock',
+  },
+  {
+    name: 'scissors',
+    emoji: '✌️',
+    beats: 'paper',
   }
+]
 
-  else if((userOne === 'rock' && userTwo === 'scissors')||
-  (userOne === 'paper' && userTwo === 'rock')||
-  (userOne === 'scissors' && userTwo === 'paper'))
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener('click', () => {
+    const selectionName = selectionButton.dataset.selection
+    const selection = SELECTIONS.find(selection => selection.name === selectionName)
+    makeSelection(selection)
+  })
+})
 
-{
-  return "Hand one wins!"
+function makeSelection(selection) {
+  const computerSelection = randomSelection()
+  const yourWinner = isWinner(selection, computerSelection)
+  const computerWinner = isWinner(computerSelection, selection)
+
+  addSelectionResult(computerSelection, computerWinner)
+  addSelectionResult(selection, yourWinner)
+
+  if (yourWinner) incrementScore(yourScoreSpan)
+  if (computerWinner) incrementScore(computerScoreSpan)
 }
 
-else if((userOne === 'rock' && userTwo === 'paper')||
-    (userOne === 'paper' && userTwo === 'scissors')||
-    (userOne === 'scissors' && userTwo === 'rock'))
+
+function incrementScore(scoreSpan) {
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function addSelectionResult(selection, winner){
+  const div = document.createElement('div')
+  div.innerText = selection.emoji
+  div.classList.add('result-selection')
+  if (winner) div.classList.add('winner')
+  finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+  return selection.beats === opponentSelection.name
+
+}
+
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+  return SELECTIONS[randomIndex]
+}
+
+// const rockPaperScissors = (hand1, hand2) => {
+
+//   let userOne = hand1.toLowerCase().trim()
+//   let userTwo = hand2.toLowerCase().trim()
+
+//   if(userOne === userTwo)
+//   {
+//     return "It's a tie!"
+//   }
+
+//   else if((userOne === 'rock' && userTwo === 'scissors')||
+//   (userOne === 'paper' && userTwo === 'rock')||
+//   (userOne === 'scissors' && userTwo === 'paper'))
+
+//   {
+//     return "Hand one wins!"
+//   }
+
+//   else if((userOne === 'rock' && userTwo === 'paper')||
+//     (userOne === 'paper' && userTwo === 'scissors')||
+//     (userOne === 'scissors' && userTwo === 'rock'))
   
-  {
-    return "Hand two wins!"
-  }
+//   {
+//     return "Hand two wins!"
+//   }
 
-  getPrompt()
-}
-  // Use the unit test to see what is expected
-
-
-
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
-function getPrompt() {
-  rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
-      getPrompt();
-    });
-  });
-}
-
-// Unit Tests
-// to use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
-
-  // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built returns the expected output.
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-    });
-    it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-    });
-    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOck', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'sCiSsOrs'), "Hand one wins!");
-    });
-  });
-} else {
-
-  // always returns ask the user for another input
-  getPrompt();
-
-}
+//   getPrompt()
+// }
